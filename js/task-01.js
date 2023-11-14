aece081542671e58c45927bdcb4416e786036b09
pbohdanov <107268135+pbohdanov@users.noreply.github.com>
2023-11-14 13:43:39 +0300

    updated task 01 and 10

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,28 @@
-const categoriesList = document.querySelector('#categories');
-const categoriesItems = categoriesList.querySelectorAll('.item');
 
-console.log(`В списке ${categoriesItems.length} категории.`);
 
-categoriesItems.forEach(item => {
-  const itemTitle = item.querySelector('h2').textContent;
-  const itemElements = item.querySelectorAll('li').length;
+const itemEl = document.querySelectorAll("#categories .item");
 
-  console.log(`Категория: ${itemTitle} / Количество элементов: ${itemElements}`);
+console.log(`Number of categories: ${itemEl.length}`);
+
+itemEl.forEach((elem, i) => {
+    const title = elem.querySelector("h2");
+    console.log("");
+    console.dir(title.textContent);
+    const liItemEl = elem.querySelectorAll("li");
+    console.dir(`Element: ${liItemEl.length}`);
 });
+
+
+
+// const categoriesList = document.querySelector('#categories');
+// const categoriesItems = categoriesList.querySelectorAll('.item');
+
+// console.log(`В списке ${categoriesItems.length} категории.`);
+
+// categoriesItems.forEach(item => {
+//   const itemTitle = item.querySelector('h2').textContent;
+//   const itemElements = item.querySelectorAll('li').length;
+
+//   console.log(`Категория: ${itemTitle} / Количество элементов: ${itemElements}`);
+// });
+
--- js/task-10.js
+++ js/task-10.js
@@ -4,27 +4,61 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const input = document.querySelector("#controls > input");
-const createBtn = document.querySelector("button[data-create]");
-const destroyBtn = document.querySelector("button[data-destroy]");
-const boxCollection = document.querySelector("#boxes");
 
-const createBoxes = (amount) => {
-  for (let i = 0; i < amount; i += 1) {
-    let newBox = document.createElement("div");
-    newBox.style.height = `${30 + 10 * i}px`;
-    newBox.style.width = `${30 + 10 * i}px`;
-    newBox.style.background = getRandomHexColor();
-    boxCollection.insertAdjacentElement("beforeend", newBox);
+const inputEl = document.querySelector("input");
+const buttonCreate = document.querySelector(`[data-create=""]`);
+const buttonDestroy = document.querySelector(`[datd-destoy=""]`);
+const boxRend = document.getElementById("boxes");
+console.log(buttonCreate);
+
+
+function createBoxes(param) {
+
+  let size = 30;
+
+  for (let i = 0; i < inputEl.value; i++) {    
+      let div = document.createElement(`div`);
+      div.style.backgroundColor = getRandomHexColor();
+      div.style.height = `${size}px`;
+      div.style.width = `${size}px`;
+      div.style.margin = `${10}px`;
+      boxRend.append(div);
+    size += 10;
   }
-};
 
-const destroyBoxes = () => {
-  boxCollection.innerHTML = "";
-};
 
-createBtn.addEventListener("click", () => {
-  createBoxes(input.value);
-});
+} 
 
-destroyBtn.addEventListener("click", destroyBoxes);
+buttonCreate.addEventListener("click", () => { createBoxes(inputEl.value) });
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// const input = document.querySelector("#controls > input");
+// const createBtn = document.querySelector("button[data-create]");
+// const destroyBtn = document.querySelector("button[data-destroy]");
+// const boxCollection = document.querySelector("#boxes");
+
+// const createBoxes = (amount) => {
+//   for (let i = 0; i < amount; i += 1) {
+//     let newBox = document.createElement("div");
+//     newBox.style.height = `${30 + 10 * i}px`;
+//     newBox.style.width = `${30 + 10 * i}px`;
+//     newBox.style.background = getRandomHexColor();
+//     boxCollection.insertAdjacentElement("beforeend", newBox);
+//   }
+// };
+
+// const destroyBoxes = () => {
+//   boxCollection.innerHTML = "";
+// };
+
+// createBtn.addEventListener("click", () => {
+//   createBoxes(input.value);
+// });
+
+// destroyBtn.addEventListener("click", destroyBoxes);
